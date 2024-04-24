ea7af34784586bae33eeff9da3c577dabf57097a
Alton <smithiialton@gmail.com>
2024-04-23 22:21:17 -0700

    added the menu and its options

--- contact_system.py
+++ contact_system.py
@@ -4,6 +4,7 @@ import os
 def contactManagement():
     print("Welcome to the Contact Management System! \nmenu:")
     print("1: Add a new Contact\n2: Edit an existing contact\n3: Delete a contact\n4: Search for a contact\n5: Display all contacts\n6: Export contacts to a text file\n7: Import contacts from a text file\n8: Quit")
+    menuOption = int(input("Choose a menu option: 1-8"))
 
 
 
