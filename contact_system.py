03c2c277750ffc35a6c6f02af089ec44188690c1
Alton <smithiialton@gmail.com>
2024-04-23 22:28:33 -0700

    added try and except for menuoption

--- contact_system.py
+++ contact_system.py
@@ -3,9 +3,22 @@ import os
 
 def contactManagement():
     print("Welcome to the Contact Management System! \nmenu:")
-    print("1: Add a new Contact\n2: Edit an existing contact\n3: Delete a contact\n4: Search for a contact\n5: Display all contacts\n6: Export contacts to a text file\n7: Import contacts from a text file\n8: Quit")
-    menuOption = int(input("Choose a menu option: 1-8"))
-
+    menu = "1: Add a new Contact\n2: Edit an existing contact\n3: Delete a contact\n4: Search for a contact\n5: Display all contacts\n6: Export contacts to a text file\n7: Import contacts from a text file\n8: Quit "
+    
+    while True:
+        print(menu)
+        try:
+            menuOption = int(input("Choose a menu option: 1-8 "))
+            if menuOption == 8:
+                print("Thank you for your service!")
+                break
+            if menuOption not in range(1,8):
+                print("Invalid Response: choice needs to be number between 1 - 8")
+            else:
+                pass
+        except ValueError:
+            print("response needs to be a integer between")
+            
 
 
 
